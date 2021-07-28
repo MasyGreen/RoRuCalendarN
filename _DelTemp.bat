8a8ff339b1aef783173242cf51292fc971afcb30
masygreen <masygreen@gmail.com>
2021-07-28 15:44:22 +0300

    icon

---
+++ _DelTemp.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+rmdir /s /q obj
+rmdir /s /q RoRuCalendarN\RoRuCalendarN\bin
+rmdir /s /q RoRuCalendarN\RoRuCalendarN\obj
+rmdir /s /q RoRuCalendarN\RoRuCalendarN.Android\bin
+rmdir /s /q RoRuCalendarN\RoRuCalendarN.Android\obj
